41c852fbad26686c22e37cf445d64e4fd5ef638d
muhammed <makhgun@gmail.com>
2023-01-02 19:32:27 +0000

    day 03

--- src/test/java/com/cydeo/tests/day3_cssSelectorXpath/T1_locators_getText.java
+++ src/test/java/com/cydeo/tests/day3_cssSelectorXpath/T1_locators_getText.java
@@ -47,7 +47,7 @@ TC #1: NextBaseCRM, locators and getText() practice
 2- Go to: https://login1.nextbasecrm.com/
 3- Enter incorrect username: “incorrect”
 4- Enter incorrect password: “incorrect”
-5- Click to login button.
+5- Click to log in button.
 6- Verify error message text is as expected:
 Expected: Incorrect login or password
 PS: Inspect and decide which locator you should be using to locate web
---
+++ src/test/java/com/cydeo/utilities/WebDriverFactory.java
@@ -0,0 +1,40 @@
+package com.cydeo.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class WebDriverFactory {
+    /*
+    // TASK: NEW METHOD CREATION
+// Method name : getDriver
+// Static method
+// Accepts String arg: browserType
+//   - This arg will determine what type of browser is opened
+//   - if "chrome" passed --> it will open Chrome browser
+//   - if "firefox" passed --> it will open firefox browser
+// RETURN TYPE: "WebDriver"
+     */
+
+    public static WebDriver getDriver(String browserType){
+
+        if (browserType.equalsIgnoreCase("chrome")){
+
+            WebDriverManager.chromedriver().setup();
+            return new ChromeDriver();
+
+        }else if (browserType.equalsIgnoreCase("firefox")){
+            WebDriverManager.firefoxdriver().setup();
+            return new FirefoxDriver();
+        }else{
+            System.out.println("Given browser type does not exist/or is not currently supported");
+            System.out.println("Driver = null");
+            return null;
+        }
+
+
+    }
+
+
+}
