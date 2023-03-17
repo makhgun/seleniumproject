66645f9d7026fee633a49b34b56f44bb1daab7d8
muhammed <makhgun@gmail.com>
2023-03-17 23:29:41 +0000

    day 06 iframes

---
+++ src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T1_WindowHandle.java
@@ -0,0 +1,73 @@
+package com.cydeo.tests.day7_webtables_utilities_javafaker;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+public class T1_WindowHandle {
+
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setUpMethod() {
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void windowHandlingTest() {
+//
+//        2. Go to : https://www.amazon.com
+        driver.get("https://www.amazon.com");
+
+//        3. Copy-paste the lines from below into your class
+        ((JavascriptExecutor) driver).executeScript("window.open('https://google.com','_blank');");
+        ((JavascriptExecutor) driver).executeScript("window.open('https://etsy.com','_blank');");
+        ((JavascriptExecutor) driver).executeScript("window.open('https://facebook.com','_blank');");
+
+//        4. Create a logic to switch to the tab where Etsy.com is open
+
+        Set<String> allWindowsHandles = driver.getWindowHandles();
+        for (String each : allWindowsHandles) {
+
+            driver.switchTo().window(each);
+
+            System.out.println("Current URL: " + driver.getCurrentUrl());
+
+            if (driver.getCurrentUrl().contains("etsy")) {
+                break;
+            }
+
+        }
+
+//        5. Assert: Title contains “Etsy”
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "Etsy";
+
+        Assert.assertTrue(actualTitle.contains(expectedTitle));
+
+
+    }
+}
+
+/*
+TC #1: Window Handle practice
+1. Create new test and make set ups
+2. Go to : https://www.amazon.com
+3. Copy paste the lines from below into your class
+4. Create a logic to switch to the tab where Etsy.com is open
+5. Assert: Title contains “Etsy”
+Lines to be pasted:
+((JavascriptExecutor) driver).executeScript("window.open('http://google.com','_blank');");
+((JavascriptExecutor) driver).executeScript("window.open('http://etsy.com','_blank');");
+((JavascriptExecutor) driver).executeScript("window.open('http://facebook.com','_blank');");
+These lines will simply open new tabs using something called JavascriptExecutor
+and get those pages. We will learn JavascriptExecutor later as well.
+ */
